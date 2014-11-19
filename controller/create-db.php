799a932fdd4f59547a81c3d048f48aafde771ddf
lizalvarez <li.alvarez760@gamil.com>
2014-11-19 13:58:06 -0800

    create-db.php 11/19/14

---
+++ controller/create-db.php
@@ -0,0 +1,5 @@
+<?php
+//accessing info from database to create
+require_once("../model/database.php");
+//path to fil. brought in variables from database
+$connection = new mysqli($host, $username, $password);
---
+++ model/database.php
@@ -0,0 +1,8 @@
+<?php
+//variables, info in our model
+$host = "localhost";
+$username = "root";
+$password = "root";
+//username and password for phpMyAdmin
+$database = "blog_db";
+
