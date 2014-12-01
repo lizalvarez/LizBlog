570e74fe5729ecbc26320efd8f8e05f2861ef8f1
lizalvarez <li.alvarez760@gamil.com>
2014-12-01 14:18:42 -0800

    checked whether the database existed and if it was successful

--- controller/create-db.php
+++ controller/create-db.php
@@ -8,10 +8,24 @@ if($connection->connect_error) {
     //if there's an error, tell the program to die off 
     die("ERROR:" . $connection->connect_error);
     //"." means concatenate
+} //if not tell program it was a success
+
+//select database that i have in the file
+$exists = $connection->select_db($database);
+//!= invert false to true
+if(!$exists){
+    //checking wether the database existed
+   //store result of query
+    $query = $connection->query("create database $database");
+    
+    if($query) {
+        echo "successfully created database: " . $database;
+    }
 }
-//if not tell program it was a success
-else {
-    //info for the host we connected to
-    echo "Success: " . $connection->host_info;
-}
+    else{
+    echo "database already exists";
+    }
+
+        
+
 $connection->close();
