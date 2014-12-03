b9693145b9d974454d74fc85fabbc6c9571194f0
lizalvarez <li.alvarez760@gamil.com>
2014-12-03 14:00:31 -0800

    created a data table

--- controller/create-db.php
+++ controller/create-db.php
@@ -6,7 +6,7 @@ $connection = new mysqli($host, $username, $password);
 //checks whether there was a connection error or not
 if($connection->connect_error) {
     //if there's an error, tell the program to die off 
-    die("ERROR:" . $connection->connect_error);
+    die("<p>ERROR:" . $connection->connect_error . "</p>");
     //"." means concatenate
 } //if not tell program it was a success
 
@@ -19,12 +19,24 @@ if(!$exists){
     $query = $connection->query("create database $database");
     
     if($query) {
-        echo "successfully created database: " . $database;
+        echo "<p>Successfully created database: " . $database . "</p>";
     }
 }
     else{
-    echo "database already exists";
+    echo "<p>Database already exists<p/>";
     }
+$query = $connection->query("CREATE TABLE posts ("
+        //eleven values in the integer,
+        . "id int(11) NOT NULL AUTO_INCREMENT," //id cant be empty, // generate unique ids
+        . "title varchar(255) NOT NULL,"//title cant be empty
+        . "post text NOT NULL,"
+        . "PRIMARY KEY (id))");  
+if($query){
+    echo"<p>Successfully create a data table: posts</p>";
+}
+else{
+    echo "<p>$connection->error</p>";
+}
 
         
 
