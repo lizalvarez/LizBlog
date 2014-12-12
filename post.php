834bd47509f84fc13f239807f4738906240ece84
lizalvarez <li.alvarez760@gamil.com>
2014-12-12 13:56:33 -0800

    created submit button

--- post.php
+++ post.php
@@ -1,7 +1,9 @@
 <?php
 require_once(__DIR__ . "/view/header.php");
 require_once(__DIR__ . "/view/form.php");
+require_once(__DIR__ . "/controller/create-db.php");
 require_once(__DIR__ . "/view/footer.php");
+
 //require post forms
 
 
--- view/form.php
+++ view/form.php
@@ -1,12 +1,17 @@
 <h1> Create Blog Post</h1>
 <form>
     <div>
-        <label for="title:"></label>
+        <label for="title">Title:</label>
         <input type="text" name="title"/>
+         </div>
+    
         <div>
             <label for="post">Post: </label>
             <textarea name="post"></textarea>
-            <!-- textarea is a big text box -->
-        </div>
+            <!-- text area is a big text box -->
+    </div>
+    
+    <div>
+        <button type="submit">Submit</button>
     </div>
 </form>
