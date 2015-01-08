2a7d36fbdf421a090128a3303d2127d12467a417
lizalvarez <li.alvarez760@gamil.com>
2015-01-08 14:05:45 -0800

    got the post to work

--- view/form.php
+++ view/form.php
@@ -1,5 +1,9 @@
+<?php
+require_once(__DIR__ . "/../model/config.php");
+//access to file
+?>
 <h1> Create Blog Post</h1>
-<form>
+<form method="post" action="<?php echo $path . "controller/create-post-php"; ?> 
     <div>
         <label for="title">Title:</label>
         <input type="text" name="title"/>
